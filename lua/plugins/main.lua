5176a88f726087dd3a25ab1faa78f027d7ade5f0
null <null>
2025-09-05 23:03:49 +0000

    tree, git, lua format

--- lua/plugins/main.lua
+++ lua/plugins/main.lua
@@ -1,1 +1,31 @@
-return {}
+return {
+  {
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
+    lazy = false,
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      require("nvim-tree").setup({})
+    end,
+  },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        lua = { "stylua" },
+      },
+      format_on_save = {
+        timeout_ms = 500,
+        lsp_fallback = true,
+      },
+    },
+  },
+  {
+    "kdheepak/lazygit.nvim",
+    keys = {
+      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+    },
+  },
+}
