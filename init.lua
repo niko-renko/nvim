02278cfd876ca10242388cad37b5182eaf1e538f
null <null>
2025-09-06 16:26:07 +0000

    java only settings

--- init.lua
+++ init.lua
@@ -1,20 +1,31 @@
+vim.diagnostic.config({
+  virtual_text = true,
+  signs = true,
+  underline = true,
+  update_in_insert = false,
+})
+
 vim.g.mapleader = " "
 
 vim.opt.termguicolors = true
 vim.opt.number = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.softtabstop = 2
 
 vim.api.nvim_create_autocmd("BufWritePre", {
-	callback = function()
-		vim.lsp.buf.format({ timeout_ms = 2000 })
-	end,
+  callback = function()
+    vim.lsp.buf.format({ timeout_ms = 2000 })
+  end,
 })
 vim.api.nvim_create_autocmd("LspAttach", {
-	callback = function(args)
-		local client = vim.lsp.get_client_by_id(args.data.client_id)
-		if client.server_capabilities.semanticTokensProvider then
-			client.server_capabilities.semanticTokensProvider = nil
-		end
-	end,
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if client.server_capabilities.semanticTokensProvider then
+      client.server_capabilities.semanticTokensProvider = nil
+    end
+  end,
 })
 
 require("config.lazy")
