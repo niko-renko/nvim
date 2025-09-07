263c3ed188eb7771d2cc059ef3753d385875c17b
null <null>
2025-09-07 17:32:45 +0000

    terminal tests

--- init.lua
+++ init.lua
@@ -28,4 +28,28 @@ vim.api.nvim_create_autocmd("LspAttach", {
   end,
 })
 
+vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
+
+_G.terminal_win = nil
+
+vim.keymap.set('n', '<leader>ti', function()
+  vim.cmd('terminal')
+  _G.terminal_win = vim.api.nvim_get_current_win()
+  vim.cmd('startinsert')
+end)
+
+vim.keymap.set('n', '<leader>tt', function()
+  if _G.terminal_win and vim.api.nvim_win_is_valid(_G.terminal_win) then
+    local current_win = vim.api.nvim_get_current_win()
+    if current_win == _G.terminal_win then
+      vim.cmd('wincmd p')
+    else
+      vim.api.nvim_set_current_win(_G.terminal_win)
+      vim.cmd('startinsert')
+    end
+  end
+end)
+
+vim.keymap.set('t', '<leader>tt', '<C-\\><C-n><C-w>p')
+
 require("config.lazy")
