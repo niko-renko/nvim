ad376dd8b6c277d8c6ff3ced952a5f78e7c97854
null <null>
2025-09-08 17:29:11 +0000

    diagnostics

--- init.lua
+++ init.lua
@@ -3,18 +3,13 @@ vim.g.mapleader = " "
 vim.opt.termguicolors = true
 vim.opt.number = true
 
-vim.api.nvim_create_autocmd("BufWritePre", {
-	callback = function()
-		vim.lsp.buf.format({ timeout_ms = 2000 })
-	end,
-})
-vim.api.nvim_create_autocmd("LspAttach", {
-	callback = function(args)
-		local client = vim.lsp.get_client_by_id(args.data.client_id)
-		if client.server_capabilities.semanticTokensProvider then
-			client.server_capabilities.semanticTokensProvider = nil
-		end
-	end,
+vim.diagnostic.config({
+	virtual_text = true,
+	signs = true,
+	underline = true,
+	update_in_insert = false,
 })
 
+require("keymap")
+require("autocmds")
 require("config.lazy")
---
+++ lua/autocmds.lua
@@ -0,0 +1,13 @@
+vim.api.nvim_create_autocmd("BufWritePre", {
+	callback = function()
+		vim.lsp.buf.format({ timeout_ms = 2000 })
+	end,
+})
+vim.api.nvim_create_autocmd("LspAttach", {
+	callback = function(args)
+		local client = vim.lsp.get_client_by_id(args.data.client_id)
+		if client.server_capabilities.semanticTokensProvider then
+			client.server_capabilities.semanticTokensProvider = nil -- rely on TreeSitter
+		end
+	end,
+})
