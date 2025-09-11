88474b870725aa6367c1feef1168f33be1212e81
null <null>
2025-09-11 21:35:48 +0000

    4 spaces for indent

--- init.lua
+++ init.lua
@@ -2,6 +2,10 @@ vim.g.mapleader = " "
 
 vim.opt.termguicolors = true
 vim.opt.number = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.softtabstop = 4
 
 vim.diagnostic.config({
 	virtual_text = true,
