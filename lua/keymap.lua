5284ca9c3985e18fd602c464fe61551a523b1e2a
null <null>
2025-09-13 02:18:54 +0000

    go to def

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -2,3 +2,4 @@ local fzf = require("fzf-lua")
 
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
 vim.keymap.set("n", "<leader>fg", fzf.live_grep, { desc = "Live Grep" })
+vim.keymap.set('n', 'gd', vim.lsp.buf.definition)
