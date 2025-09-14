64f953290e08064d3e55425358909c1086494133
null <null>
2025-09-14 20:03:58 +0000

    small improvements

--- lua/servers/jdtls.lua
+++ lua/servers/jdtls.lua
@@ -1,40 +1,38 @@
 local home = vim.fn.expand("~")
 local group = vim.api.nvim_create_augroup("jdtls", {})
 
-local open_classfile = function(fname)
-    local buf = vim.api.nvim_get_current_buf()
-    vim.bo[buf].modifiable = true
-    vim.bo[buf].swapfile = false
-    vim.bo[buf].buftype = 'nofile'
-    vim.bo[buf].filetype = 'java'
-    local client = vim.tbl_filter(function(c)
-        return c.name == 'jdtls'
-    end, vim.lsp.get_clients())[1]
+local open_classfile = function(uri)
+    local bufnr = vim.api.nvim_get_current_buf()
+    vim.api.nvim_buf_set_option(bufnr, "buftype", "nofile")
+    vim.api.nvim_buf_set_option(bufnr, "modifiable", true)
+    vim.api.nvim_buf_set_option(bufnr, "swapfile", false)
+    vim.api.nvim_buf_set_option(bufnr, "filetype", "java")
 
-    local content
-    local function handler(err, result)
+    local done = false
+    local params = { uri = uri.match }
+    local client = vim.tbl_filter(function(c)
+        return c.name == "jdtls"
+    end, vim.lsp.get_clients())[1]
+    local function handler(err, content)
         assert(not err, vim.inspect(err))
-        assert(result, "jdtls client must return result for java/classFileContents")
-        content = result
-        local normalized = string.gsub(result, '\r\n', '\n')
+        assert(content, "jdtls client must return result for java/classFileContents")
+        local normalized = string.gsub(content, "\r\n", "\n")
         local source_lines = vim.split(normalized, "\n", { plain = true })
-        vim.api.nvim_buf_set_lines(buf, 0, -1, false, source_lines)
-        vim.bo[buf].modifiable = false
+        -- vim.bo[bufnr].modifiable = false
+        vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, source_lines)
+        vim.api.nvim_buf_set_option(bufnr, "modifiable", false)
+        done = true
     end
-    local params = {
-        uri = fname
-    }
-    client:request("java/classFileContents", params, handler, buf)
-    vim.wait(1000, function() return content ~= nil end)
+
+    client:request("java/classFileContents", params, handler, bufnr)
+    vim.wait(1000, function() return done == true end)
 end
 
 
 vim.api.nvim_create_autocmd("BufReadCmd", {
     group = group,
     pattern = "jdt://*",
-    callback = function(args)
-        open_classfile(args.match)
-    end
+    callback = open_classfile
 })
 
 vim.lsp.config["jdtls"] = {
