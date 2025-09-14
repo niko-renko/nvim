3efed53df3536e2929f860edd51237d1f0809c72
null <null>
2025-09-14 17:54:53 +0000

    the only needed cap?

--- lua/servers/jdtls.lua
+++ lua/servers/jdtls.lua
@@ -1,26 +1,42 @@
 local home = vim.fn.expand("~")
 local caps = {
-    actionableRuntimeNotificationSupport = true,
-    advancedExtractRefactoringSupport = true,
-    advancedGenerateAccessorsSupport = true,
-    advancedIntroduceParameterRefactoringSupport = true,
-    advancedOrganizeImportsSupport = true,
-    advancedUpgradeGradleSupport = true,
     classFileContentsSupport = true,
-    clientDocumentSymbolProvider = true,
-    clientHoverProvider = false,
-    executeClientCommandSupport = true,
-    extractInterfaceSupport = true,
-    generateConstructorsPromptSupport = true,
-    generateDelegateMethodsPromptSupport = true,
-    generateToStringPromptSupport = true,
-    gradleChecksumWrapperPromptSupport = true,
-    hashCodeEqualsPromptSupport = true,
-    inferSelectionSupport = { "extractConstant", "extractField", "extractInterface", "extractMethod", "extractVariableAllOccurrence", "extractVariable" },
-    moveRefactoringSupport = true,
-    onCompletionItemSelectedCommand = "editor.action.triggerParameterHints",
-    overrideMethodsPromptSupport = true
 }
+local uri_from_jdt = function(uri, callback)
+    local client = vim.lsp.get_active_clients({ name = "jdtls" })[1]
+    if not client then return end
+
+    client.request(
+        "workspace/executeCommand",
+        {
+            command = "java.contentProvider",
+            arguments = { uri }
+        },
+        function(err, result)
+            if err or not result then return end
+            -- open a new scratch buffer with the retrieved content
+            local bufnr = vim.api.nvim_create_buf(true, false)
+            vim.api.nvim_buf_set_option(bufnr, "buftype", "nofile")
+            vim.api.nvim_buf_set_name(bufnr, uri)
+            vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, vim.split(result, "\n"))
+            if callback then callback(bufnr) end
+        end,
+        0
+    )
+end
+local definition_handler = function(err, result, ctx, config)
+    print("here")
+    if not result or vim.tbl_isempty(result) then return end
+    local location = result[1]
+    local uri = location.uri
+    if uri:match("^jdt://") then
+        uri_from_jdt(uri, function(bufnr)
+            vim.api.nvim_set_current_buf(bufnr)
+        end)
+    else
+        vim.lsp.util.jump_to_location(location, "utf-8", true)
+    end
+end
 vim.lsp.config["jdtls"] = {
     cmd = {
         "jdtls",
@@ -31,6 +47,9 @@ vim.lsp.config["jdtls"] = {
     root_markers = { ".git", "build.gradle", "build.gradle.kts", "build.xml", "pom.xml", "settings.gradle", "settings.gradle.kts" },
     init_options = {
         extendedClientCapabilities = caps
+    },
+    handlers = {
+        ['textDocument/definition'] = definition_handler
     }
 }
 vim.lsp.enable("jdtls")
