6e553581d73d4f1393842718ce57f683d1def843
null <null>
2025-09-18 11:05:43 +0000

    command to toggle nvim tree

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -1,6 +1,8 @@
 local fzf = require("fzf-lua")
+local nvim_tree = require("nvim-tree.api")
 
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
 vim.keymap.set("n", "<leader>fg", fzf.live_grep)
+vim.keymap.set("n", "<C-n>", nvim_tree.tree.toggle)
 
 vim.keymap.set("n", "gd", vim.lsp.buf.definition)
