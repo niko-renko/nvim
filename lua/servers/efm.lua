b539d6dca81e716cc2f71754774316aae630e3a8
null <null>
2025-09-20 18:23:43 +0000

    another checkstyle format

--- lua/servers/efm.lua
+++ lua/servers/efm.lua
@@ -1,7 +1,10 @@
 local checkstyle = {
     rootMarkers = { ".git/" },
     lintCommand = "checkstyle -c config/checkstyle/checkstyle.xml -f plain ${INPUT}",
-    lintFormats = { "[%tARN] %f:%l:%c: %m [%.%#]" },
+    lintFormats = {
+        "[%tARN] %f:%l:%c: %m [%.%#]",
+        "[%tARN] %f:%l: %m [%.%#]"
+    },
     lintAfterOpen = true,
     lintStdin = false,
     lintIgnoreExitCode = true
