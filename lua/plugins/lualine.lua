dcc1716343d6bc05ef00e522fbcad8440760dcad
null <null>
2025-11-15 19:16:38 +0000

    lualine relative path

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,7 +1,15 @@
 return {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    opts = {
-        options = { globalstatus = true }
+  'nvim-lualine/lualine.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  opts = {
+    options = { globalstatus = true },
+    sections = {
+      lualine_c = {
+        {
+          'filename',
+          path = 1
+        }
+      }
     }
+  }
 }
