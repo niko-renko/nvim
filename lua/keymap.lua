04ac294e40f208591207564af7dd32be9885e9ab
null <null>
2025-12-05 16:16:20 +0000

    new binding

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -3,6 +3,7 @@ local nvim_tree = require("nvim-tree.api")
 
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
 vim.keymap.set("n", "<leader>fg", fzf.live_grep)
+vim.keymap.set("n", "<leader>ff", fzf.files)
 vim.keymap.set("n", "<C-n>", nvim_tree.tree.toggle)
 
 vim.keymap.set("n", "gd", vim.lsp.buf.definition)
