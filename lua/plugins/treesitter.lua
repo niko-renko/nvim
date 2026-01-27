3614746480188f2d3f39d2634dd1fb31830359ce
null <null>
2026-01-27 15:15:36 -0500

    new treesitter api

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,10 +2,9 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function()
-        require("nvim-treesitter.configs").setup({
+        require("nvim-treesitter").setup({
             auto_install = true,
-            highlight = { enable = true },
-            indent = { enable = false },     -- rely on LSP
+            indent = { enable = false }, -- rely on LSP
         })
     end,
 }
