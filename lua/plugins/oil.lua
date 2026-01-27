3948704bf498c95ccdc590fbd87eded1bc8c5d04
null <null>
2026-01-27 15:28:56 -0500

    use oil

---
+++ lua/plugins/oil.lua
@@ -0,0 +1,35 @@
+return {
+    "stevearc/oil.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    lazy = false,
+    keys = {
+        { "-", "<cmd>Oil<cr>", desc = "Open parent directory" },
+        { "<leader>fo", "<cmd>Oil<cr>", desc = "Open Oil file browser" },
+    },
+    opts = {
+        default_file_explorer = true,
+        delete_to_trash = true,
+        skip_confirm_for_simple_edits = true,
+        view_options = {
+            show_hidden = true,
+        },
+        keymaps = {
+            ["g?"] = "actions.show_help",
+            ["<CR>"] = "actions.select",
+            ["<C-v>"] = "actions.select_vsplit",
+            ["<C-s>"] = "actions.select_split",
+            ["<C-t>"] = "actions.select_tab",
+            ["<C-p>"] = "actions.preview",
+            ["<C-c>"] = "actions.close",
+            ["<C-r>"] = "actions.refresh",
+            ["-"] = "actions.parent",
+            ["_"] = "actions.open_cwd",
+            ["`"] = "actions.cd",
+            ["~"] = "actions.tcd",
+            ["gs"] = "actions.change_sort",
+            ["gx"] = "actions.open_external",
+            ["g."] = "actions.toggle_hidden",
+            ["g\\"] = "actions.toggle_trash",
+        },
+    },
+}
