afe42ef5ddef0b96de4ef9a49f4acc2048fbdff2
null <null>
2026-03-18 13:28:14 -0400

    get rid of fidget

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -9,7 +9,29 @@ return {
           'filename',
           path = 1
         }
-      }
+      },
+      lualine_x = {
+        {
+          function()
+            local clients = vim.lsp.get_clients({ bufnr = 0 })
+            if #clients == 0 then
+              return ""
+            end
+            local names = {}
+            for _, client in ipairs(clients) do
+              local progress = vim.lsp.status()
+              if progress and progress ~= "" then
+                return progress
+              end
+              table.insert(names, client.name)
+            end
+            return table.concat(names, ", ")
+          end,
+        },
+        'encoding',
+        'fileformat',
+        'filetype',
+      },
     }
   }
 }
